5e8cb9f0f6574fa5909f31df36e5a1da9a9ced39
Philippe Beck <philippe@philippebeck.net>
2019-10-03 14:36:09 +0200

    Create HomeController.php

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
+
+/**
+ * Class HomeController
+ * @package App\Controller
+ */
+class HomeController extends MainController
+{
+    /**
+     * @return string
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function indexAction()
+    {
+        return $this->render('home.twig');
+    }
+}
