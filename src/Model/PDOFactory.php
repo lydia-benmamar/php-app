85dc155872c9f2bc55e0ebcda12df48713b39804
Philippe Beck <philippe@philippebeck.net>
2019-10-03 14:58:02 +0200

    Create PDOFactory.php

---
+++ src/Model/PDOFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Model;
+
+use PDO;
+
+/**
+ * Class PDOFactory
+ * @package App\Model
+ */
+class PDOFactory
+{
+    /**
+     * @var null
+     */
+    private static $pdo = null;
+    /**
+     * @return PDO|null
+     */
+    public static function getPDO()
+    {
+        require_once '../config/db.php';
+
+        if (self::$pdo === null) {
+            self::$pdo = new PDO(DB_DSN, DB_USER, DB_PASS, DB_OPTIONS);
+            self::$pdo->exec('SET NAMES UTF8');
+        }
+
+        return self::$pdo;
+    }
+}
