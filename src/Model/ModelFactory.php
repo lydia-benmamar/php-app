0a4fd560f4f6ab2877fdbf0e4600e05eaccb682a
Philippe Beck <philippe@philippebeck.net>
2019-10-03 19:09:58 +0200

    Create ModelFactory.php

---
+++ src/Model/ModelFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Model;
+
+/**
+ * Class ModelFactory
+ * @package App\Model
+ */
+class ModelFactory
+{
+    /**
+     * @var array
+     */
+    private static $models = [];
+
+    /**
+     * @param $table
+     * @return mixed
+     */
+    public static function getModel(string $table)
+    {
+        if (array_key_exists($table, self::$models)) {
+            return self::$models[$table];
+        }
+
+        $class = 'App\Model\\' . ucfirst($table) . 'Model';
+        self::$models[$table] = new $class(new PDODatabase(PDOFactory::getPDO()));
+
+        return self::$models[$table];
+    }
+}
